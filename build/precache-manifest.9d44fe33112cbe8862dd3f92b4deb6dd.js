bf3e9a0246d30be270d3541b2951c41989bb3eb3
Pratik Mathur <pmathur@totalmortgage.com>
2020-07-02 19:19:56 -0400

    build push

---
+++ build/precache-manifest.9d44fe33112cbe8862dd3f92b4deb6dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95cc9cafec22b9a6780f6022b8da1dfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2780390e2a6047882a7",
+    "url": "/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "3cc1739e7bb5c8146eb8",
+    "url": "/static/css/main.9a111a24.chunk.css"
+  },
+  {
+    "revision": "c2780390e2a6047882a7",
+    "url": "/static/js/2.f084b2b1.chunk.js"
+  },
+  {
+    "revision": "d9d85c294e473a87c1286ef93bab2cc2",
+    "url": "/static/js/2.f084b2b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cc1739e7bb5c8146eb8",
+    "url": "/static/js/main.f4bcec84.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  },
+  {
+    "revision": "8715c355143720a32918db119650e80d",
+    "url": "/static/media/cover.8715c355.png"
+  },
+  {
+    "revision": "ddc2dbf77f665d35e091d2cff37ac3e9",
+    "url": "/static/media/cover_2.ddc2dbf7.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-website-v2/precache-manifest.6c6e8d6c9ad9cdd5cbcc1d20a6c49c6d.js"
+  "/precache-manifest.9d44fe33112cbe8862dd3f92b4deb6dd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-website-v2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
