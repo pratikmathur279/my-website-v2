61d5a5847c0c2d369359da958a6cc8c6d446ea4e
Pratik Mathur <pratikmathur279@gmail.com>
2022-02-16 00:35:07 -0500

    proxy server

--- src/actions/Actions.js
+++ src/actions/Actions.js
@@ -1,92 +1,80 @@
-import axios from 'axios';
+import Dispatcher from '../dispatcher/dispatcher';
+import Request from 'superagent';
+import Actions from "./Action";
 
-class Actions {
+class MyActions extends Actions {
+    constructor() {
+        super();
+    }
+
+
     getSkills(skills, callback) {
-        axios('https://n6j1yr1b43.execute-api.us-east-1.amazonaws.com/dev/skills', {
-            crossDomain: true
-        })
-            .then((res) => {
-                var data = (res.data);
-                //   console.log(data);
-                data.sort((a, b) => (a.index > b.index) ? 1 : -1);
-                callback(data);
-            });
+        this.get('/api/skills', (err, res) => {
+            var data = (res.body);
+            data.sort((a, b) => (a.index > b.index) ? 1 : -1);
+            callback(data);
+        });
     }
 
     sendContact(contact, callback) {
         // console.log(contact);
-        axios.post('https://n6j1yr1b43.execute-api.us-east-1.amazonaws.com/dev/contact', contact, { crossDomain: true })
-            .then((res, err) => {
-                if (!err) {
-                    console.log("data saved!");
-                    callback(true);
-                }
-
-            })
+        this.post('/api/contact', contact, (res, err) => {
+            if (!err) {
+                console.log("data saved!");
+                callback(true);
+            }
+        });
     }
 
     sendEmail(email, callback) {
         // console.log(email);
-        axios.post('https://n6j1yr1b43.execute-api.us-east-1.amazonaws.com/dev/email/send', email, { crossDomain: true })
-            .then((res, err) => {
-                if (!err) {
-                    console.log("data saved!");
-                    callback(true);
-                }
-
-            })
+        this.post('/api/send-email', email, (res, err) => {
+            if (!err) {
+                console.log("data saved!");
+                callback(true);
+            }
+        });
     }
 
     getExperience(experience, callback) {
-        axios('https://n6j1yr1b43.execute-api.us-east-1.amazonaws.com/dev/experience', {
-            crossDomain: true
-        })
-            .then((res) => {
-                var data = res.data;
-                // console.log(data);
-                for (var i in data) {
-                    var temp = data[i].responsibilities;
-                    temp = temp.split(";");
-                    data[i].responsibilities = temp;
-                    // console.log(data.length);
-                    if (i == data.length - 1) {
-                        data.sort((a, b) => (a.index > b.index) ? 1 : -1);
-                        callback(data);
-                    }
+        this.get('/api/experiences', (err, res) => {
+            var data = res.body;
+            // console.log(data);
+            for (var i in data) {
+                var temp = data[i].responsibilities;
+                temp = temp.split(";");
+                data[i].responsibilities = temp;
+                // console.log(data.length);
+                if (i == data.length - 1) {
+                    data.sort((a, b) => (a.index > b.index) ? 1 : -1);
+                    callback(data);
                 }
-            })
+            }
+        });
     }
 
     getProjects(experience, callback) {
-        axios('/api/projects', {
-            crossDomain: true
-        })
-            .then((res) => {
-                var data = res.data;
-                // console.log(data);
-                data.sort((a, b) => (a.index > b.index) ? 1 : -1);
-                callback(data);
-            })
+        this.get('/api/projects', (err, res) => {
+            var data = res.body;
+
+            data.sort((a, b) => (a.index > b.index) ? 1 : -1);
+            callback(data);
+        });
     }
 
     getTechnology(tech, callback) {
-        axios('https://n6j1yr1b43.execute-api.us-east-1.amazonaws.com/dev/technology', {
-            crossDomain: true
-        })
-            .then((res) => {
-                var data = res.data;
-                data.sort((a, b) => (a.index > b.index) ? 1 : -1);
-                callback(data);
-            })
+        this.get('/api/technology', (err, res) => {
+            var data = res.body;
+            data.sort((a, b) => (a.index > b.index) ? 1 : -1);
+            callback(data);
+        });
     }
 
     getInstagramFeed(feed, callback) {
-        axios('https://api.instagram.com/oembed?url=' + feed)
-            .then((res) => {
-                callback(res.data);
-            });
-
+        this.get('https://api.instagram.com/oembed?url=' + feed, (err, res) => {
+            callback(res.body);
+        })
     }
 }
 
-export default Actions;
+export default MyActions;
